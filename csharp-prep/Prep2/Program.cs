a16973c2ddc10ef2763f942e5fccd490e04f5862
g3nke11 <gisaack@byui.edu>
2024-09-19 16:36:26 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int perc = int.Parse(answer);
+
+        string letter = "";
+
+        if (perc >= 90)
+        {
+            letter = "A";
+        }
+        else if (perc >= 80)
+        {
+            letter = "B";
+        }
+        else if (perc >= 70)
+        {
+            letter = "C";
+        }
+        else if (perc >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+        if (perc >= 70)
+        {
+            Console.WriteLine("You passed! Congratulation!");
+        }
+        else 
+        {
+            Console.WriteLine("Too bad. Better luck next time.");
+        }
     }
 }
