3a8d1975720ad592a6a77bf3e2876ef7f2364022
g3nke11 <gisaack@byui.edu>
2024-09-24 16:15:34 -0600

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input_num = 1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (input_num != 0)
+        {
+        Console.Write("Enter number: ");
+        string answer = Console.ReadLine();
+        input_num = int.Parse(answer);
+        if (input_num != 0)
+        {
+        numbers.Add(input_num);
+        }
+        }
+
+        int sum_num = 0;
+        int largest_num = 0;
+        int smallest_pos_num = 999999999;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum_num += numbers[i];
+            if (largest_num < numbers[i])
+            {
+                largest_num = numbers[i];
+            }
+            if (smallest_pos_num > numbers[i] && numbers[i] > 0)
+            {
+                smallest_pos_num = numbers[i];
+            }
+        }
+        float avg_num = ((float)sum_num) / numbers.Count;
+        numbers.Sort();
+        
+        Console.WriteLine($"The sum is: {sum_num}");
+        Console.WriteLine($"The average is: {avg_num}");
+        Console.WriteLine($"The largest number is: {largest_num}");
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
