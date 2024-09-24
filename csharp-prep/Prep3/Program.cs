9f5e649c2da90061fc218b1abdd52e43173a5425
g3nke11 <gisaack@byui.edu>
2024-09-23 18:03:46 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+        int guess_int = -1;
+
+        while (guess_int != magic_number)
+        {
+        Console.Write("What is your guess? ");
+        string answer = Console.ReadLine();
+        guess_int = int.Parse(answer);
+        if (guess_int < magic_number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess_int > magic_number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        }
     }
 }
