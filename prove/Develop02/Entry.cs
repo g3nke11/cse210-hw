452afa38cd64e94f539be24bb598cd434c0aea64
g3nke11 <gisaack@byui.edu>
2024-10-11 17:39:14 -0600

    Updates to Journal for creativity

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,11 +1,13 @@
 public class Entry
 {
+    public string _writer;
     public string _date;
     public string _prompt;
     public string _response;
 
     public void Display()
     {
+        Console.WriteLine($"Writer: {_writer}");
         Console.WriteLine($"Date: {_date} - Prompt: {_prompt}");
         Console.WriteLine($"{_response}\n");
     }
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -7,15 +7,17 @@ public class Journal
     public void AddEntry()
     {
         Prompt prompts = new Prompt();
-        prompts._prompts.Add("What was the most enjoyable part of your day, and why did it stand out to you?");
-        prompts._prompts.Add("Did you face any challenges today, and how did you handle them?");
-        prompts._prompts.Add("What is something you learned or discovered today, no matter how small?");
-        prompts._prompts.Add("How did you take care of yourself today, either physically or mentally?");
+        prompts._prompts.Add("What was the most enjoyable part of your day and why did it stand out to you?");
+        prompts._prompts.Add("Did you face any challenges today and how did you handle them?");
+        prompts._prompts.Add("What is something you learned or discovered today?");
+        prompts._prompts.Add("How did you take care of yourself today either physically or mentally?");
         prompts._prompts.Add("Was there a moment today that made you feel proud or accomplished?");
-        prompts._prompts.Add("What was your mood like throughout the day, and did anything significantly affect it?");
+        prompts._prompts.Add("What was your mood like throughout the day and did anything significantly affect it?");
         prompts._prompts.Add("Did you connect with someone today in a meaingful way and how did that interaction make you feel?");
-        
+
         Entry entry = new Entry();
+        Console.Write("Please enter your name: ");
+        entry._writer = Console.ReadLine();
         DateTime theCurrentTime = DateTime.Now;
         entry._date = theCurrentTime.ToShortDateString();
         entry._prompt = prompts.GetRandomPrompt();
@@ -39,6 +41,7 @@ public class Journal
         string filename = Console.ReadLine();
         using (StreamWriter outputFile = new StreamWriter(filename))
         {
+            outputFile.WriteLine("Date,Prompt,Response");
             foreach(Entry entry in _entries)
             {
                 outputFile.WriteLine($"{entry._date},{entry._prompt},{entry._response}");
@@ -50,8 +53,9 @@ public class Journal
     {
         Console.Write("What is the filename? ");
         string filename = Console.ReadLine();
-        string[] lines = System.IO.File.ReadAllLines(filename);
+        string[] lines1 = System.IO.File.ReadAllLines(filename);
         _entries.Clear();
+        string[] lines = lines1.Skip(1).ToArray();
 
         foreach(string line in lines)
         {
